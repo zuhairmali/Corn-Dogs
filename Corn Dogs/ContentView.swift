e91fe1d58ac8ede2bf8726b9f4ae59af4fd5b3da
Zuhair Ali <zuhairali20003@gmail.com>
2021-05-10 00:06:01 -0500

    Initial Commit

---
+++ Corn Dogs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Corn Dogs
+//
+//  Created by Student on 5/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Corn Dogs/Corn_DogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Corn_DogsApp.swift
+//  Corn Dogs
+//
+//  Created by Student on 5/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Corn_DogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
